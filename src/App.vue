39924266466322dd304df5f983602f39e1848f6a
Azmin Zamsanirani <azamsanirani@Lenovo.com>
2025-04-29 16:48:45 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  background-color: #f9fafb; // light background
+  font-family: 'Inter', sans-serif;
+}
+</style>
